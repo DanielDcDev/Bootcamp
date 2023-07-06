1a3e381b6a6d6defbf81d4ac9b98195456c8eda4
Dc Dev <96603256+DanielDcDev@users.noreply.github.com>
2023-07-06 22:17:53 +0100

    update aula 01

---
+++ JavaPOO/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello World!, my babyes");
+
+        byte byteValue = 127;
+        //short do -32767 ao 32767;
+        //char 0 a 65535;
+        //int -2b a 2b;
+        //long -9t ao 9t;
+        //boolean true false;
+
+
+        System.out.println(byteValue);
+    }
+}
